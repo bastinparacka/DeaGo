4004373316f272f7c7a0f353aae4a8c0c3905cb1
Sebin Benjamin <sebinbenjamin@gmail.com>
2016-02-11 23:49:06 +0530

    trying to use fb-graph

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var FB = require('fb');
-
+var accessToken = FB.getAccessToken();
+FB.setAccessToken(accessToken);
+/*
 FB.api('oauth/access_token', {
     client_id: '1601573453438037',
     client_secret: 'd8e292eb44e7924f6d5690836b6630b5',
     grant_type: 'client_credentials'
 }, function (res) {
     if(!res || res.error) {
-        console.log(!res ? 'error occurred' : res.error);
+        console.log(!res ? 'error occurred in client login' : res.error);
         return;
     }
- 
     var accessToken = res.access_token;
 });
+*/
 
 FB.api('4', { fields: ['id', 'name'] }, function (res) {
   if(!res || res.error) {
-    console.log(!res ? 'error occurred' : res.error);
+    console.log(!res ? 'error occurred getting fb data' : res.error);
     return;
   }
   console.log(res.id);
