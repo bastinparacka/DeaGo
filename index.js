f61757239e72b37d9c7cc58ee659d23e81e20f30
Sebin Benjamin <sebinbenjamin@gmail.com>
2016-02-11 23:11:08 +0530

    Added App secret

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var FB = require('fb');
 
 FB.api('oauth/access_token', {
     client_id: '1601573453438037',
-    client_secret: '',
+    client_secret: 'd8e292eb44e7924f6d5690836b6630b5',
     grant_type: 'client_credentials'
 }, function (res) {
     if(!res || res.error) {
