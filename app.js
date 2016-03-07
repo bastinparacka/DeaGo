59d5706ab910a01f97fefddaf9a1d11b0bd4440b
Sebin Benjamin <sebinbenjamin@gmail.com>
2016-03-08 00:46:30 +0530

    Hello Facebook....nodes got youuuu git add . -A

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-var express       = require('express');
-/*    FB            = require('fb'),
+
+var express       = require('express'),
+    FB            = require('fb'),
     http          = require('http'),
     path          = require('path'),
     config        = require('./config'),
     api           = require('./routes/api'),
     home          = require('./routes/home'),
     meals         = require('./routes/meals');
-*/
+
 var app = express();
-/*
+
 if(!config.facebook.appId || !config.facebook.appSecret) {
     throw new Error('facebook appId and appSecret required in config.js');
 }
-*/
+
 app.configure(function() {
-	
-	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8081);
-	app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
     app.use(express.favicon());
@@ -28,7 +27,7 @@ app.configure(function() {
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
-/*
+
 app.configure('development', function() {
     app.use(express.errorHandler());
 });
@@ -41,7 +40,7 @@ app.get( '/friends',         api.friends);
 app.post('/announce',        api.announce);
 app.get( '/meals/:id',       meals.show);
 app.get( '/winjs/meals/:id', meals.showWinJs); // this is used for facebook-winjs-sdk sample and not required to actually run this sample
-*/
-http.createServer(app).listen(app.get('port'),app.get('ip'),function() {
+
+http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 var config = { };
 
 // should end in /
-config.rootUrl  = process.env.ROOT_URL = "http://"+process.env.OPENSHIFT_APP_DNS || "http://localhost";
+config.rootUrl  = process.env.ROOT_URL                  || 'http://localhost:3000/';
+
 config.facebook = {
-    appId:          process.env.FACEBOOK_APPID          || '1516393198679725',
-    appSecret:      process.env.FACEBOOK_APPSECRET      || 'de91d55b88a764cea4f580a3e234288d',
-    appNamespace:   process.env.FACEBOOK_APPNAMESPACE   || 'DeaGo',
+    appId:          process.env.FACEBOOK_APPID          || '1571953956456982',
+    appSecret:      process.env.FACEBOOK_APPSECRET      || '42f39acd8b1c69d8a335ee3adc2cea5c',
+    appNamespace:   process.env.FACEBOOK_APPNAMESPACE   || 'hello_fb',
     redirectUri:    process.env.FACEBOOK_REDIRECTURI    ||  config.rootUrl + 'login/callback'
 };
 
 module.exports = config;
-
